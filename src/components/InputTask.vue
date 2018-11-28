135b31f72dffb9bf36f7971a3235637258f45cbb
Willian Ribeiro <willian.ribeiro@gauge.com.br>
2018-11-28 18:01:07 -0200

    Implement fake add_task function on InputTask.vue

--- src/components/InputTask.vue
+++ src/components/InputTask.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-    <input type="text" class="new-todo" placeholder="O que precisa ser feito?">
+    <input
+      @keyup.enter="add_task"
+      v-model="message"
+      type="text"
+      class="new-todo"
+      placeholder="O que precisa ser feito?"
+    >
+    <label for="">{{message}}</label>
   </div>
 </template>
 
 <script>
+import { Task } from '../models/Task'
+
 export default {
   data () {
-    return {}
+    return {
+      message: ''
+    }
+  },
+
+  methods: {
+    add_task ($event) {
+      const value = $event.target.value
+      const task = new Task()
+      task.completed = false
+      task.title = value
+      console.log('task', task)
+    }
   }
 }
 </script>
