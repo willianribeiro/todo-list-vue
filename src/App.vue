98a4ce0f5132e761ae81f9be3d6e191340b866bc
Willian Ribeiro <willian.ribeiro@gauge.com.br>
2018-11-28 14:14:24 -0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <section class="todoapp">
+      <header class="header">
+        <h1>Tarefas</h1>
+        <input-task></input-task>
+      </header>
+    </section>
+  </div>
+</template>
+
+<script>
+import InputTask from './components/InputTask'
+
+export default {
+  name: 'App',
+  components: {
+    InputTask
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/InputTask.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="text" class="new-todo" placeholder="O que precisa ser feito?">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
